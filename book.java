6f7ad3fb2b58fe4186a254aae664114f78166a28
Priyankagh2003 <117583004+Priyankagh2003@users.noreply.github.com>
2023-01-27 00:26:52 +0530

    Update book.java

--- book.java
+++ book.java
@@ -1,77 +1,81 @@
-import java.util.Scanner;
-	
+import java.io.*;
+import java.util.*;
 
-	 class Book{
-	
 
-	   int num_pages;
-	   double price;
-	  String name;
-	  String author;
-	   Book(){
-	    num_pages=0;
-	    price=0.0;
-	    name="some_book";
-	    author="amshu";
-	  }
-	   Book(int num_pages,double price, String name, String author){
-	    this.num_pages=num_pages;
-	    this.price=price;
-	    this.name=name;
-	    this.author=author;
-	  }
-	  void set_data(int num_pages,double price, String name, String author)  {
-	       this.num_pages=num_pages;
-	       this.price=price;
-	       this.name=name;
-	       this.author=author;
-	
 
-	  }
-	  void get_data(){
-	    System.out.println("Book details\nname: "+name+"\nauthor: "+author+"\nnmber of pages: "+num_pages+"\nprice: "+price);
-	    System.out.println("\n------------------------\n");
-	  }
-	  public String toString(){
-	    return ("Book details\nname: "+name+"\nauthor: "+author+"\nnmber of pages: "+num_pages+"\nprice: "+price+"\n---------------------------------\n");
-	  }
-	
+class Book {
 
-	  
-	  }
-	class lab_2 {
-	  public static void main(String[] args) {
-	    Book b1=new Book();
-	    Scanner s=new Scanner(System.in);
-	    System.out.print("eneter the name of the book: ");
-	    String name=s.nextLine();
-	    System.out.print("eneter the author's name: ");
-	    String author=s.nextLine();
-	    System.out.print("eneter the number of pages in the book: ");
-	    int  num_pages=s.nextInt();
-	    System.out.print("eneter the price of the book: ");
-	    double price=s.nextDouble();
-	    System.out.println();
-	    b1.set_data(num_pages,price,name,author);
-	    Book b2=new Book(20,87.65,"deception point","dan brown");
-	    b1.get_data();
-	    b2.get_data();
-	    System.out.println(b1);
-	    System.out.println(b2);
-	    s.close();
-	
+	String title, author;
+	double price;
+	int numPages;
 
-	            
-	    
-	  }
-	
+	Book() {
+		
+		title="Default"; 
+		author="Default";
+		price=0.0;
+		numPages=0;
+	}
+	void setTitle(String t) {
 
+		title=t; 
+		
+	}
+	void setAuthor(String a) {
+		
 	
-
+		author=a;
 	
-
+	}
+	void setPrice(double p) {
+		
+		
+		price=p;
+		
+	}
+	void setPages(int np) {
+		
+	
+		numPages=np;
 	}
 
+	public String toString() {
 
+		return title+"\t"+author+"\t"+price+"\t"+numPages+"\n";
+	}
+}
 
+class BookDetails {
 
+	public static void main(String args[]) {
+		String t, a;
+		double p;
+		int np,n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of Books");
+		n = sc.nextInt();
+		Book b[]= new Book[n];
+ 		for(int i=0; i<n;i++) {
+			System.out.println("Enter the Title of  the Books");			
+			t= sc.next();
+			System.out.println("Enter the Author of  the Books");		
+			a= sc.next();
+			System.out.println("Enter the Price of  the Books");				
+			p= sc.nextDouble();
+			System.out.println("Enter the Number of pages of  the Books");	
+			np= sc.nextInt();
+					
+			b[i] = new Book();
+			b[i].setTitle(t);
+			b[i].setAuthor(a);
+			b[i].setPrice(p);
+			b[i].setPages(np);
+		}
+
+		System.out.println("Title \t Author \t Price \t Pages\n");
+		for(int i=0; i<n;i++) {
+			System.out.println(b[i]);
+		}
+		
+	}
+}
